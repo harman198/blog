f40fb19cf4a834d430e63488fd4a6d121163f8cf
Harman Khangura <harman198@gmail.com>
2024-06-25 23:37:45 +1000

    Feature: Add some styling classes to page layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
+
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 export default {
+	darkMode: 'selector',
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: 'var(--color-primary)',
+				secondary: 'var(--color-secondary)',
+				accent: 'var(--color-accent)',
+				default: 'var(--color-text-default)',
+				muted: 'var(--color-text-muted)',
+			},
+			fontFamily: {
+				sans: ['var(--font-sans, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
+				serif: ['var(--font-serif, ui-serif)', ...defaultTheme.fontFamily.serif],
+				heading: ['var(--font-heading, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
+			},
+		},
 	},
 	plugins: [
 		require('@tailwindcss/typography'),
